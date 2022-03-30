87ae66062e6d3b556427235efb4e43015b29ec12
Mumuni Mohammed <mumunim10@gmail.com>
2022-03-30 23:38:42 +0000

    Added security group to allow web traffic and ssh

--- main.tf
+++ main.tf
@@ -110,3 +110,42 @@ resource "aws_route_table_association" "route_association" {
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index + 1)
   route_table_id = aws_route_table.flugel_route_table.id
 }
+
+# Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.flugel_vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
